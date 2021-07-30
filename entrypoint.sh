3cc427b8f941a8af8e1540d7244b5bcd5fd498a0
Hermerson Araújo <ne.hermerson@gmail.com>
2021-07-30 16:34:16 -0300

    chore: improve entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 
+AWS_ACCESS_KEY_ID="$1"
+AWS_SECRET_ACCESS_KEY="$2"
+STAGE="$3"
+REGION="$4"
+
 npm install
 
-npx sls config credentials -p aws --key {{AWS_ACCESS_KEY_ID}} --secret {{AWS_SECRET_ACCESS_KEY}}
+npx sls config credentials -p aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
-npx sls deploy -v
+npx sls deploy --stage $STAGE --region $REGION -v
 
